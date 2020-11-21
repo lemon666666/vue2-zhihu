a531cd12365576db6980909ed0f46f306555ff26
lemon666666 <1848999832@qq.com>
2020-11-21 23:53:52 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,4 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lemon666666.github.io/vue2-zhihu.git main:gh-pages
+git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
